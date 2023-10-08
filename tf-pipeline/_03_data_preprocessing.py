a7103ccf3fdca2d45783804fce6d3b49c98d6611
Mohamad Noureldine <132208495+mohamadnoureldine89@users.noreply.github.com>
2023-10-08 22:13:30 +0200

    ML Pipeline works

--- tf-pipeline/_03_data_preprocessing.py
+++ tf-pipeline/_03_data_preprocessing.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
 
 def preprocess_data(img_size=48, batch_size=64):
     train_datagen = ImageDataGenerator(rotation_range = 180,
@@ -15,8 +16,8 @@ def preprocess_data(img_size=48, batch_size=64):
     validation_split = 0.2)
 
     # path to local directory
-    train_dir = os.path.join('train')
-    test_dir = os.path.join('test')
+    train_dir = os.path.join('tf-pipeline/train')
+    test_dir = os.path.join('tf-pipeline/test')
 
     train_generator = train_datagen.flow_from_directory(directory = train_dir,
     target_size = (img_size,img_size),
@@ -33,6 +34,8 @@ def preprocess_data(img_size=48, batch_size=64):
     class_mode = "categorical",
     subset = "validation"
     )
+
+    return train_generator, validation_generator
 
     
 
