271ad02dda47ac8298bd46e399479cc8526c8e50
Mohamad Noureldine <132208495+mohamadnoureldine89@users.noreply.github.com>
2023-10-08 16:29:32 +0200

    added data load

--- tf-pipeline/_01_data_ingestion.py
+++ tf-pipeline/_01_data_ingestion.py
@@ -1,7 +1,16 @@
 import numpy as np
 import pandas as pd
 import os
+import kaggle
 
 def load_data():
-    
-    pass
+ 
+    # Kaggle identifiers are defined in Github as secrets
+
+    # Download the dataset by specifying the dataset name
+    dataset_name = 'msambare/fer2013'
+    kaggle.api.dataset_download_files(dataset_name, path='tf-pipeline', unzip=True)
+
+
+
+load_data()
